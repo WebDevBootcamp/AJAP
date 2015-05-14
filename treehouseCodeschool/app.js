23cd6a604e139e303dc628a29955402d1ae9cb62
Klammertime <audrey.ck@gmail.com>
2015-05-13 20:26:28 -0600

    Changed app.js file so that there is a wrapper around both school functions

--- treehouseCodeschool/app.js
+++ treehouseCodeschool/app.js
@@ -1,15 +1,27 @@
 $(document).ready(function(){
 	$("#coding-form").submit(function() {
-	var username = $("#username").val();
-	getBadges(username, ".pointsLessons");
-	//console.log(username);
+	var usernameTreehouse = $("#usernameTreehouse").val();
+	var usernameCodeschool = $("#usernameCodeschool").val();
+	runBadges(usernameTreehouse, usernameCodeschool, ".pointsLessons");
+	console.log(usernameTreehouse, usernameCodeschool);
 	return false;
 	});
 });
 
-function getBadges(username, element) {
+function runBadges(usernameTreehouse, usernameCodeschool, element){
+	$(element).html("");
+	if (usernameTreehouse){
+		getBadges(usernameTreehouse, element);
+	}
+	if (usernameCodeschool) {
+		getBadges2(usernameCodeschool, element);
+	}
+}
+
+
+function getBadges(usernameTreehouse, element) {
 		$.ajax({
-		url: "http://teamtreehouse.com/" + username + ".json",
+		url: "http://teamtreehouse.com/" + usernameTreehouse + ".json",
 		type: "GET",
 		crossDomain: true,
 		dataType: "json",
@@ -17,7 +29,6 @@ function getBadges(username, element) {
 		success: function(dataBack) {
 			//console.log(dataBack);
 			var badges = dataBack.badges;
-			// $(element).html("");
 			$(element).append("<h3>I have taken " + badges.length + " lessons and scored " + dataBack.points.total + 
 				" points at Treehouse!</h3><div class=\"badges\"></div>");
 			badges.forEach(function(badge, i){
@@ -25,11 +36,8 @@ function getBadges(username, element) {
 					$(".badges").append("<li class=\"badgeImages\"> <img src=' " + badge.icon_url + " '/></li>");
 				}
 			});
-			//for(var j = 0; j < Math.min(badges.length, 7); j++) {
-			// 	var badge = badges[j];
-			// 	console.log(badge);
-			// 	$(".badges").append("<li class=\"badgeImages\"> <img src=' " + badge.icon_url + " '/></li>");
-			// }
+			//had called it from within but now using a wrapper runBadges
+			//getBadges2(username, element);
 		},
 		error: function(dataBack) {
 			console.log("something went wrong.");
@@ -38,19 +46,19 @@ function getBadges(username, element) {
 	});
 }
 
+// 	now that we are using runBadges we don't need this
+// 	$(document).ready(function(){
+// 	$("#coding-form").submit(function() {
+// 	var username2 = $("#username2").val();
+// 	getBadges2(username2, ".pointsLessons");
+// 	console.log(username2);
+// 	return false;
+// 	});
+// });
 
-$(document).ready(function(){
-	$("#coding-form").submit(function() {
-	var username2 = $("#username2").val();
-	getBadges2(username2, ".pointsLessons");
-	console.log(username2);
-	return false;
-	});
-});
-
-function getBadges2(username2, element) {
+function getBadges2(usernameCodeschool, element) {
 		$.ajax({
-		url: "https://www.codeschool.com/users/" + username2 + ".json",
+		url: "https://www.codeschool.com/users/" + usernameCodeschool + ".json",
 		type: "GET",
 		crossDomain: true,
 		dataType: "jsonp",
@@ -65,6 +73,7 @@ function getBadges2(username2, element) {
 					$(".badges2").append("<li class=\"badgeImages\"> <img src=' " + badge.badge + " '/></li>");
 			 	}
 			});
+			//was using this for loop but decided on a foreach instead
 			//for(var j = 0; j < Math.min(badges.length, 7); j++) {
 			// 	var badge = badges[j];
 			// 	console.log(badge);
