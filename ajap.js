e919b89b4cefb08a1e1fa04840e02f7bd31f7cc6
Aaron Goodman <Amgoodman5@gmail.com>
2015-05-16 12:11:04 -0600

    <some styling added on jumbotron and table>

--- ajap.js
+++ ajap.js
@@ -3,43 +3,9 @@
  */
 $(document).ready(function(){
 
-    $("#go").click(function(){ getInfo(); });
-
-});
-
-
-function getInfo(){
-    var url = "http://teamtreehouse.com/"+ encodeURIComponent($('#name').val())+".json";
-
-    console.log(url);
-
-
-    $.ajax({
-        url: url,
-        context: document.body,
-
-    })
-        .done(function(data){
-
-            var userName= data.name;
-            var totalPoints=data.points.total;
-            var image = new Image;
-            image.src = data.gravatar_url;
-            var totalBadges=data.badges.length;
-
-            var img = $('<img src="' + image.src + '" "height=200 width=200" />');
-
-
-            $("#userInfo").html("<strong> Name: </strong>"+userName+"</br><strong> Total Points: </strong>"+totalPoints+"</br> <strong> Total Badges: </strong>"+totalBadges+"</br>" );
-
-            $("#img").append(img);
-
-            console.log(data);
-            console.log(image);
+    $(""
 
 
 
-        });
-}
 
 
